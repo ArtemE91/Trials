84d134b192bea2d0964d9ac91d4ad05cdf9df57a
cofa-ai <66384139+cofa-ai@users.noreply.github.com>
2020-06-13 01:12:28 +0300

    Передеал TrialList аналогично SampleList Начал работу по экспериментам

--- Trial/views.py
+++ Trial/views.py
@@ -1,6 +1,26 @@
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from Sample.models import Trials
-from .form import TrialForm, TrialUpdateForm
+from .form import TrialForm, TrialUpdateForm, ExperementForm
+from django.http import JsonResponse
+
+
+class AjaxableResponseMixin:
+    def form_invalid(self, form):
+        response = super().form_invalid(form)
+        if self.request.is_ajax():
+            return JsonResponse(form.errors, status=400)
+        else:
+            return response
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        if self.request.is_ajax():
+            data = {
+                'pk': self.object.pk,
+            }
+            return JsonResponse(data, status=200)
+        else:
+            return response
 
 
 class TrialCreate(CreateView):
@@ -19,6 +39,16 @@ class TrialDetail(DetailView):
     template_name = 'Trial/trial_detail.html'
 
 
+class TrialModalDetail(DetailView):
+    model = Trials
+    template_name = 'Trial/trial_modal_detail.html'
+
+
+class TrialDetailTr(AjaxableResponseMixin, DetailView):
+    model = Trials
+    template_name = 'Trial/trial_detail_tr.html'
+
+
 class TrialUpdate(UpdateView):
     model = Trials
     template_name = 'Trial/trial_update.html'
@@ -29,3 +59,8 @@ class TrialUpdate(UpdateView):
 class TrialDelete(DeleteView):
     model = Trials
     success_url = '/trial/'
+
+
+class ExperimentCreate(CreateView):
+    form_class = ExperementForm
+    template_name = 'Experiment/Experiment.html'
