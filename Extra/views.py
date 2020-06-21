4f4f4620591649ed0049975616bde0db52dcbe49
ArtemE91 <temik91Efremov@gmail.com>
2020-06-21 19:47:13 +0300

    Добавил проверку exceptions в загрузку данных через файл.

--- Extra/views.py
+++ Extra/views.py
@@ -1,9 +1,10 @@
-import os
 import yaml
+import json
 
 from django.views.generic import TemplateView
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import redirect, render
+from django.db import transaction
 
 from Sample.models import Sample, SampleMaterial, SampleType, Trials, ReceivedValues
 
@@ -15,81 +16,122 @@ class DataLoading:
                   'sample': {},
                   'trials': {},
                   'experiment': {}}
+    error_message = {'block': '', 'error_message': ''}
 
     def __init__(self, config, author_id):
-        self.author_id = author_id
-        self.sample_type = config['sample_type']
-        self.sample_material = config['sample_material']
-        self.sample = config['sample']
-        self.trial = config['trials']
-        self.experiment = config['received_values']
+        try:
+            self.author_id = author_id
+            self.sample_type = config['sample_type']
+            self.sample_material = config['sample_material']
+            self.sample = config['sample']
+            self.trial = config['trials']
+            self.experiment = config['received_values']
+        except Exception as e:
+            self.error_message['block'] = 'Не заполнены основные поля в файле для загрузки данных.' \
+                                          ' Ознакомтесь с инструкцией заполнения!'
+            self.error_message['error_message'] = e
 
     def write_data(self):
-        self.save_type()
-        self.save_material()
-        self.save_sample()
-        self.save_trial()
-        self.save_experiment()
-        return self.data_write
+        try:
+            self.save_type()
+            self.save_material()
+            self.save_sample()
+            self.save_trial()
+            self.save_experiment()
+            return self.data_write
+        except ValueError as e:
+            self.error_message['error_message'] = e
+            return self.error_message
 
     def save_type(self):
-        for type_id in self.sample_type:
-            self.sample_type[type_id][self.author] = self.author_id
-            try:
-                self.data_write['sample_type'][type_id] = SampleType.objects.get(**self.sample_type[type_id])
-            except ObjectDoesNotExist:
-                self.data_write['sample_type'][type_id] = SampleType.objects.create(**self.sample_type[type_id])
+        try:
+            for type_id in self.sample_type:
+                self.error_message['block'] = json.dumps(self.sample_type[type_id])
+                self.sample_type[type_id][self.author] = self.author_id
+                try:
+                    self.data_write['sample_type'][type_id] = SampleType.objects.get(**self.sample_type[type_id])
+                except ObjectDoesNotExist:
+                    self.data_write['sample_type'][type_id] = SampleType.objects.create(**self.sample_type[type_id])
+        except Exception as e:
+            raise ValueError(e)
 
     def save_material(self):
-        for material_id in self.sample_material:
-            self.sample_material[material_id][self.author] = self.author_id
-            try:
-                self.data_write['sample_material'][material_id] = SampleMaterial.objects.get(
-                    **self.sample_material[material_id])
-            except ObjectDoesNotExist:
-                self.data_write['sample_material'][material_id] = SampleMaterial.objects.create(
-                    **self.sample_material[material_id])
+        try:
+            for material_id in self.sample_material:
+                self.error_message['block'] = json.dumps(self.sample_material[material_id])
+                self.sample_material[material_id][self.author] = self.author_id
+                try:
+                    self.data_write['sample_material'][material_id] = SampleMaterial.objects.get(
+                        **self.sample_material[material_id])
+                except ObjectDoesNotExist:
+                    self.data_write['sample_material'][material_id] = SampleMaterial.objects.create(
+                        **self.sample_material[material_id])
+        except Exception as e:
+            raise ValueError(e)
 
     def save_sample(self):
-        for sample_id in self.sample:
-            self.sample[sample_id][self.author] = self.author_id
+        try:
+            for sample_id in self.sample:
+                self.error_message['block'] = json.dumps(self.sample[sample_id])
+                self.sample[sample_id][self.author] = self.author_id
 
-            id_material = self.data_write['sample_material'][self.sample[sample_id]['sample_material_id']].id
-            self.sample[sample_id]['sample_material_id'] = id_material
+                id_material = self.data_write['sample_material'][self.sample[sample_id]['sample_material_id']].id
+                self.sample[sample_id]['sample_material_id'] = id_material
 
-            id_type = self.data_write['sample_type'][self.sample[sample_id]['sample_type_id']].id
-            self.sample[sample_id]['sample_type_id'] = id_type
+                id_type = self.data_write['sample_type'][self.sample[sample_id]['sample_type_id']].id
+                self.sample[sample_id]['sample_type_id'] = id_type
 
-            self.data_write['sample'][sample_id] = Sample.objects.create(**self.sample[sample_id])
+                self.data_write['sample'][sample_id] = Sample.objects.create(**self.sample[sample_id])
+        except Exception as e:
+            raise ValueError(e)
 
     def save_trial(self):
-        for trials_id in self.trial:
-            self.trial[trials_id][self.author] = self.author_id
+        try:
+            for trials_id in self.trial:
+                self.error_message['block'] = json.dumps(self.trial[trials_id])
+                self.trial[trials_id][self.author] = self.author_id
 
-            sample_id = self.data_write['sample'][self.trial[trials_id]['sample_id']].id
-            self.trial[trials_id]['sample_id'] = sample_id
+                sample_id = self.data_write['sample'][self.trial[trials_id]['sample_id']].id
+                self.trial[trials_id]['sample_id'] = sample_id
 
-            self.data_write['trials'][trials_id] = Trials.objects.create(**self.trial[trials_id])
+                self.data_write['trials'][trials_id] = Trials.objects.create(**self.trial[trials_id])
+        except Exception as e:
+            raise ValueError(e)
 
     def save_experiment(self):
-        for exp_id in self.experiment:
-            self.experiment[exp_id][self.author] = self.author_id
+        try:
+            for exp_id in self.experiment:
+                self.error_message['block'] = json.dumps(self.experiment[exp_id])
+                self.experiment[exp_id][self.author] = self.author_id
 
-            trials_id = self.data_write['trials'][self.experiment[exp_id]['trials_id']].id
-            self.experiment[exp_id]['trials_id'] = trials_id
+                trials_id = self.data_write['trials'][self.experiment[exp_id]['trials_id']].id
+                self.experiment[exp_id]['trials_id'] = trials_id
 
-            self.data_write['experiment'][exp_id] = ReceivedValues.objects.create(**self.experiment[exp_id])
+                self.data_write['experiment'][exp_id] = ReceivedValues.objects.create(**self.experiment[exp_id])
+        except Exception as e:
+            raise ValueError(e)
 
 
 class DataLoadingTemplateView(TemplateView):
     http_method_names = ['get', 'post']
     template_name = 'extra/data_loading.html'
 
+    @transaction.atomic
     def post(self, request, *args, **kwargs):
-        # В дальниешем сделать проверку на размер файла
-        config = yaml.safe_load(request.FILES['file'].read())
-        data_loading = DataLoading(config, request.user.id)
-        context = data_loading.write_data()
-        return render(request, self.template_name, {'sample': context['sample'],
-                                                    'trials': context['trials'],
-                                                    'experiment': context['experiment']})
+        # В дальнейшем сделать проверку на размер файла
+        try:
+            config = yaml.safe_load(request.FILES['file'].read())
+        except Exception as e:
+            context = {'value': 'Ошибка в чтении файла! Проверьте формат файла.',
+                       'error': e}
+        else:
+            data_loading = DataLoading(config, request.user.id)
+            data = data_loading.write_data()
+            if 'error_message' in data:
+                context = {'title': 'Блок в котором возникли ошибки: ',
+                           'value': data['block'], 'error': data['error_message']}
+            else:
+                context = {'sample': data['sample'], 'trials': data['trials'],
+                           'experiment': data['experiment']}
+        finally:
+            return render(request, self.template_name, context)
